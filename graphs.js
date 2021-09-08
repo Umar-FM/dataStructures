b6c616a06a8882bf401731e61bacdd8cd75d7d8f
Umar-FM <=>
2021-09-08 12:02:44 -0400

    implemented breath-first and depth-first traversal algorithms

---
+++ graphs.js
@@ -0,0 +1,48 @@
+const depthFirstPrint = (graph,source) => {
+    const stack = [source];
+
+    while (stack.length > 0){
+        const current = stack.pop();
+        console.log(current)
+        for (let neighbor of graph[current]){
+            stack.push(neighbor)
+        }
+    }
+};
+
+const depthFirstPrintRecursive = (graph,source) => {
+    console.log(source);
+    for(let neighbor of graph[source]){
+        depthFirstPrintRecursive(graph,neighbor)
+    }
+}
+
+const breadthFirstPrint = (graph,source) =>{
+    const queue = [source];
+
+    while(queue.length > 0){
+        const current = queue.shift();
+        console.log(current);
+        for(let neighbor of graph[current]){
+            queue.push(neighbor)
+        }
+    }
+}
+
+const graph = {
+    a: ['c','b'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+};
+
+console.log("Depth First Print")
+depthFirstPrint(graph,'a'); //abdfce
+
+console.log("Depth First Print - Recursive")
+depthFirstPrintRecursive(graph,'a'); //acebdf
+
+console.log("Breadth First Print")
+breadthFirstPrint(graph,'a') // acbedf
